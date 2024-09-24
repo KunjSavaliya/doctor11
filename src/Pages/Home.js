ae0ea68133a64c7dd547315d8a00c0f29354a4a0
KunjSavaliya <kunj.bvminfotech@gmail.com>
2024-09-25 03:30:04 +0530

    first commit

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -38,7 +38,7 @@ function Home() {
             <div className="relative w-full h-[400px] -mt-2 ">
                 <img src={images[currentImage]} alt="carousel" className="w-full h-full object-cover" />
                 <div className=" flex flex-col gap-10 absolute top-0 left-0 p-5 text-white">
-                    <h2 className="text-5xl w-[70%] font-bold pl-36 animate__animated animate__fadeInDown">{textContent[currentImage]}</h2>
+                    <h2 className="text-5xl w-[70%] font-bold lg:pl-36 animate__animated animate__fadeInDown">{textContent[currentImage]}</h2>
                     <div className='pl-36'>
                         <button className=" text-white font-bold bg-[#14B0EB] px-5 py-4 rounded animate__animated animate__fadeInUp">BOOK AN APPOINTMENT </button>
 
@@ -59,7 +59,7 @@ function Home() {
             </div>
             <div className="flex flex-wrap "> {/* Main div with 50% width */}
                 {/* Left Section with Text */}
-                <div className="flex-1  w-[50%] bg-[#f3f5f9] p-8 pl-32 pr-32">
+                <div className="flex-1  w-[50%] bg-[#f3f5f9] p-8 lg:pl-32 lg:pr-32">
                     <p className="font-normal text-gray-300">Top Keyhole Heart Surgeon in Ahmedabad</p>
                     <p className="font-semibold text-4xl text-blue-700 mt-3">Dr. Jaydip Ramani - Best Cardiac Surgeon in Ahmedabad</p>
                     <p className='text-gray-600 mt-5 text-justify'>
@@ -105,14 +105,14 @@ function Home() {
 
             <div className='bg-[#001e57] '>
                 <h1 className='text-4xl pt-10 mb-10 font-bold text-center text-white '>Our Specialization</h1>
-                <div className='pl-36 pr-36 flex flex-wrap justify-around gap-6'>
+                <div className='lg:pl-36 lg:pr-36 flex flex-wrap justify-around gap-6'>
                     {/* First Card */}
                     <div className='flex flex-col items-center'>
                         <div className='bg-white border border-black p-2  animate__animated animate__pulse'> {/* Added p-2 for padding */}
                             <img src={hp9} alt='Vascular Surgeon' width={200} height={100} className=' border-black  border-[5px] h-[200px] ' />
                         </div>
                         <p className='text-xl text-white mt-3'>Best Vascular Surgeon in Ahmedabad</p>
-                        <p className='text-[#14B0EB] w-80 text-center mt-5'>At Plexus Heart Surgery Centre, we understand the profound impact heart health concerns can have on your life......</p>
+                        <p className='text-[#14B0EB] lg:w-80 text-center mt-5'>At Plexus Heart Surgery Centre, we understand the profound impact heart health concerns can have on your life......</p>
                         <p className='text-[#14B0EB]'>Read more</p>
                     </div>
 
@@ -123,7 +123,7 @@ function Home() {
                             <img src={hp8} alt='Vascular Surgeon' width={200} height={100} className=' border-black  border-[5px] h-[200px] ' />
                         </div>
                         <p className='text-xl text-white mt-3'>Heart Valve Surgeries</p>
-                        <p className='text-[#14B0EB] w-80 text-center mt-5'>At Plexus Heart Surgery Centre, we understand the critical role healthy heart valves play in maintaining a well-functioning cardiovascular system.....</p>
+                        <p className='text-[#14B0EB] lg:w-80 text-center mt-5'>At Plexus Heart Surgery Centre, we understand the critical role healthy heart valves play in maintaining a well-functioning cardiovascular system.....</p>
                         <p className='text-[#14B0EB]'>Read more</p>
                     </div>
                     <div className='flex flex-col items-center'>
@@ -131,7 +131,7 @@ function Home() {
                             <img src={hp7} alt='Vascular Surgeon' width={200} height={100} className=' border-black  border-[5px] h-[200px] ' />
                         </div>
                         <p className='text-xl text-white mt-3'>Minimally Invasive CABG</p>
-                        <p className='text-[#14B0EB] w-80 text-center mt-5'>Traditionally, coronary artery bypass grafting (CABG) has involved open-heart surgery with a large incision through the chest.....</p>
+                        <p className='text-[#14B0EB] lg:w-80 text-center mt-5'>Traditionally, coronary artery bypass grafting (CABG) has involved open-heart surgery with a large incision through the chest.....</p>
                         <p className='text-[#14B0EB]'>Read more</p>
                     </div>
                     <div className='flex flex-col items-center'>
@@ -139,7 +139,7 @@ function Home() {
                             <img src={hp6} alt='Vascular Surgeon' width={200} height={100} className=' border-black  border-[5px] h-[200px] ' />
                         </div>
                         <p className='text-xl text-white mt-3'>Mitral Valve Replacement</p>
-                        <p className='text-[#14B0EB] w-80 text-center mt-5'>The mitral valve plays a vital role in ensuring smooth blood flow within the heart. When this valve malfunctions, it can significantly impact your heart's ability to pump blood efficiently....</p>
+                        <p className='text-[#14B0EB] lg:w-80 text-center mt-5'>The mitral valve plays a vital role in ensuring smooth blood flow within the heart. When this valve malfunctions, it can significantly impact your heart's ability to pump blood efficiently....</p>
                         <p className='text-[#14B0EB]'>Read more</p>
                     </div>
                     <div className='flex flex-col items-center'>
@@ -147,7 +147,7 @@ function Home() {
                             <img src={hp5} alt='Vascular Surgeon' width={200} height={100} className=' border-black  border-[5px] h-[200px] ' />
                         </div>
                         <p className='text-xl text-white mt-3'>Coronary Artery Bypass</p>
-                        <p className='text-[#14B0EB] w-80 text-center mt-5'>Plexus Heart Surgery Centre is a leading cardiac care provider in Ahmedabad. It offers comprehensive treatment options for coronary artery disease, including coronary artery bypass surgery (CABG).....</p>
+                        <p className='text-[#14B0EB] lg:w-80 text-center mt-5'>Plexus Heart Surgery Centre is a leading cardiac care provider in Ahmedabad. It offers comprehensive treatment options for coronary artery disease, including coronary artery bypass surgery (CABG).....</p>
                         <p className='text-[#14B0EB]'>Read more</p>
                     </div>
                     <div className='flex flex-col items-center'>
@@ -155,7 +155,7 @@ function Home() {
                             <img src={hp4} alt='Vascular Surgeon' width={200} height={100} className=' border-black  border-[5px] h-[200px] ' />
                         </div>
                         <p className='text-xl text-white mt-3'>L.V. Aneurysms Operation</p>
-                        <p className='text-[#14B0EB] w-80 text-center mt-5'>The left ventricle, your heart's main pumping chamber, can become weakened and bulge outward due to a condition called left ventricular aneurysm (LVA).....</p>
+                        <p className='text-[#14B0EB] lg:w-80 text-center mt-5'>The left ventricle, your heart's main pumping chamber, can become weakened and bulge outward due to a condition called left ventricular aneurysm (LVA).....</p>
                         <p className='text-[#14B0EB]'>Read more</p>
                     </div>
                     <div className='flex flex-col items-center'>
@@ -163,7 +163,7 @@ function Home() {
                             <img src={hp3} alt='Vascular Surgeon' width={200} height={100} className=' border-black  border-[5px] h-[200px] ' />
                         </div>
                         <p className='text-xl text-white mt-3'>Aortic root surgery (Aneurysms)</p>
-                        <p className='text-[#14B0EB] w-80 text-center mt-5'>The aortic root, the very foundation of your aorta, plays a critical role in directing blood flow from your heart to the rest of your body.....</p>
+                        <p className='text-[#14B0EB] lg:w-80 text-center mt-5'>The aortic root, the very foundation of your aorta, plays a critical role in directing blood flow from your heart to the rest of your body.....</p>
                         <p className='text-[#14B0EB]'>Read more</p>
                     </div>
                     <div className='flex flex-col items-center'>
@@ -171,7 +171,7 @@ function Home() {
                             <img src={hp2} alt='Vascular Surgeon' width={200} height={100} className=' border-black  border-[5px] h-[200px] ' />
                         </div>
                         <p className='text-xl text-white mt-3'>Coronary Endarterectomy</p>
-                        <p className='text-[#14B0EB] w-80 text-center mt-5'>While bypass surgery has become a mainstay for treating blocked coronary arteries, there's another option for certain patients: coronary endarterectomy.....
+                        <p className='text-[#14B0EB] lg:w-80 text-center mt-5'>While bypass surgery has become a mainstay for treating blocked coronary arteries, there's another option for certain patients: coronary endarterectomy.....
                             Read more </p>
                         <p className='text-[#14B0EB]'>Read more</p>
                     </div>
@@ -180,7 +180,7 @@ function Home() {
                             <img src={hp1} alt='Vascular Surgeon' width={200} height={100} className=' border-black  border-[5px] h-[200px] ' />
                         </div>
                         <p className='text-xl text-white mt-3'>Myxoma</p>
-                        <p className='text-[#14B0EB] w-80 text-center mt-5'>While most heart conditions involve blockages or weakened muscles, myxomas present a unique challenge. These rare, non-cancerous tumors arise from the heart's connective tissue and can disrupt blood flow within the chambers....</p>
+                        <p className='text-[#14B0EB] lg:w-80 text-center mt-5'>While most heart conditions involve blockages or weakened muscles, myxomas present a unique challenge. These rare, non-cancerous tumors arise from the heart's connective tissue and can disrupt blood flow within the chambers....</p>
                         <p className='text-[#14B0EB]'>Read more</p>
                     </div>
 
@@ -188,7 +188,7 @@ function Home() {
             </div>
             <div className="flex flex-wrap justify-center item-center "> {/* Main div with 50% width */}
                 {/* Left Section with Text */}
-                <div className="flex-1  w-[50%] bg-[#f3f5f9] p-8 pl-32 pr-32">
+                <div className="flex-1  w-[50%] bg-[#f3f5f9] p-8 lg:pl-32 lg:pr-32">
                     <p className="font-semibold text-4xl text-black mt-3">Our Expert Doctor</p>
                     <p className=' mt-5 text-blue-700 text-xl text-justify'>
                         Approach
