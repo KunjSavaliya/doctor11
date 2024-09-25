55cbc4f52ec7f45600abbce69d3571b4b1b5a7ed
KunjSavaliya <kunj.bvminfotech@gmail.com>
2024-09-25 16:25:38 +0530

    header change

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -10,51 +10,57 @@ function Contact() {
     const handleHomeClick = () => {
         navigate('/');
     };
+
     return (
         <div>
-            <div className="relative w-full h-[400px] -mt-2">
+            {/* Banner Section */}
+            <div className="relative w-full h-[300px] md:h-[400px] -mt-2">
                 <img src={doctor} alt="carousel" className="w-full h-full object-cover" />
-                <div className="flex flex-col gap-10 absolute top-0 left-0 p-5 text-white h-[100%] justify-center items-start">
-                    <h2 className="text-4xl lg:text-5xl font-bold lg:pl-36">Contact us</h2>
-                    <div className='lg:pl-36 flex gap-3'>
-                        <p className='text-xl text-[#14B0EB] font-medium cursor-pointer' onClick={handleHomeClick}>
+                <div className="flex flex-col gap-6 absolute top-0 left-0 p-5 text-white h-full justify-center items-start lg:pl-36">
+                    <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold">Contact us</h2>
+                    <div className="flex gap-2 text-lg md:text-xl">
+                        <p className="text-[#14B0EB] font-medium cursor-pointer" onClick={handleHomeClick}>
                             Home
                         </p>
-                        <p className='font-bold text-xl'> {'>'} Get In Touch</p>
+                        <p className="font-bold">{'>'} Get In Touch</p>
                     </div>
                 </div>
             </div>
-            <div className='pl-[5%] pr-[5%] mt-10 xl:pl-[20%] xl:pr-[20%]'>
+
+            {/* Contact and Map Section */}
+            <div className="px-5 md:px-[10%] xl:px-[20%] mt-10">
                 <div className="flex flex-col lg:flex-row gap-10">
-
-                    <div className='bg-[#14B0EB] p-10 w-full lg:w-1/2 animate__animated animate__fadeInLeft' >
-                        <div className='mt-6'>
-                            <p className='text-white font-bold text-3xl'>Contact Details</p>
-                            <p className='text-[#fffc]'>Get in touch with us for any questions about our industries or projects.</p>
-                            <div className='flex gap-3 mt-6'>
-                                <CiLocationOn className='mb-2 text-[#fffc] size-30 lg:size-[72px]' />
+                    {/* Contact Details */}
+                    <div className="bg-[#14B0EB] p-8 md:p-10 w-full lg:w-1/2 animate__animated animate__fadeInLeft">
+                        <div>
+                            <p className="text-white font-bold text-2xl md:text-3xl">Contact Details</p>
+                            <p className="text-[#fffc] mt-3">Get in touch with us for any questions about our industries or projects.</p>
+                            
+                            <div className="flex gap-3 mt-6 items-center">
+                                <CiLocationOn className="text-[#fffc] text-3xl md:text-4xl" />
+                                <p className="text-[#fffc]">Rosevill sky Opp Croma, Nr. Shukan cross road, Nikol - Naroda Rd, Ahmedabad, Gujarat 382346</p>
+                            </div>
+                            
+                            <div className="flex gap-3 mt-6 items-center">
+                                <HiOutlineMailOpen className="text-[#fffc] text-3xl md:text-4xl" />
                                 <div>
-
-                                    <p className='text-[#fffc]'>Rosevill sky Opp Croma, Nr. Shukan cross road, Nikol - Naroda Rd, Ahmedabad, Gujarat 382346</p>
+                                    <p className="text-[#fffc] font-bold">Email us</p>
+                                    <p className="text-[#fffc]">info@drjaydipramanivascularsurgeon.com</p>
                                 </div>
                             </div>
-                            <div className='flex gap-3 mt-6'>
-                                <HiOutlineMailOpen size={30} className='mb-2 text-[#fffc]' />
+
+                            <div className="flex gap-3 mt-6 items-center">
+                                <MdAddIcCall className="text-[#fffc] text-3xl md:text-4xl" />
                                 <div>
-                                    <p className='text-[#fffc] font-bold'>Email us</p>
-                                    <p className='text-[#fffc]'>info@drjaydipramanivascularsurgeon.com</p>
-                                </div>
-                            </div>
-                            <div className='flex gap-3 mt-6'>
-                                <MdAddIcCall size={30} className='mb-2 text-[#fffc]' />
-                                <div>
-                                    <p className='text-[#fffc] font-bold'>Call Support</p>
-                                    <p className='text-[#fffc]'>7862058448</p>
+                                    <p className="text-[#fffc] font-bold">Call Support</p>
+                                    <p className="text-[#fffc]">7862058448</p>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div className="w-full lg:w-1/1 h-[450px] mt-10 lg:mt-0 animate__animated animate__fadeInRight">
+
+                    {/* Google Map */}
+                    <div className="w-full lg:w-1/2 h-[300px] md:h-[450px] animate__animated animate__fadeInRight">
                         <iframe
                             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3670.102117734681!2d72.62462677609533!3d23.07664148490971!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e87d7a385ab7f%3A0x428ae743f3ae5f4c!2sRoseville%20Sky!5e0!3m2!1sen!2sin!4v1695654992847!5m2!1sen!2sin"
                             width="100%"
@@ -73,3 +79,4 @@ function Contact() {
 }
 
 export default Contact;
+    
